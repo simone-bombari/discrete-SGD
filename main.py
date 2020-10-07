9150d2a1859d67db9a1b8753ffc5a4f724a71918
Simone BOMBARI <sbombari@lngrad319.ist.local>
2020-10-07 20:07:26 +0200

    First examples with single_well_1d

--- main.py
+++ main.py
@@ -4,39 +4,56 @@ import matplotlib.pyplot as plt
 from matplotlib.offsetbox import AnchoredText
 import utils
 import losses
+from datetime import datetime
 
 matplotlib.rcParams['mathtext.fontset'] = 'cm'
 matplotlib.rcParams['font.family'] = 'STIXGeneral'
 
 
-N = 5000000
-sigma = 0.0001
+def dynamics_and_plot(param, N):
+    eta, a, sigma = param
+    positions = np.zeros(N)
+    for i in range(1, N):
+        noise = eta * np.random.normal(0, sigma)
+        positions[i] = positions[i - 1] + noise - eta * losses.grad_single_well_1d(positions[i - 1], a)
+
+    x = np.linspace(-6 * sigma * np.sqrt(eta / a), 6 * sigma * np.sqrt(eta / a), 1000)
+    y = losses.single_well_1d(x, a)
+    p = utils.pdf_single_well_1d(x, sigma, eta, a)
+    fig, ax = plt.subplots()
+    fig.set_size_inches(3, 2)
+    bins = 500
+    ax.hist(positions, bins=bins, density=True, label=r'$p_d(x)$')
+    ax.plot(x, p, 'r', label=r'$p_c(x)$')
+    ax.plot(x, y, 'k', linestyle='--', label=r'$L(x)$')
+    ax.legend(loc="upper right")
+    ax.set_xlabel(r'$x$')
+    ax.set_ylim([0, 5 * max(p) / 4])
+    ax.set_xlim([-4 * sigma * np.sqrt(eta / a), 4 * sigma * np.sqrt(eta / a)])
+    plt.tick_params(
+        axis='both',
+        which='both',
+        bottom=False,
+        left=False,
+        labelbottom=False,
+        labelleft=False)
+    anchored_text = AnchoredText(r'$\eta={}$'.format(eta) + '\n' + r'$a={}$'.format(a) +
+                                 '\n' + r'$\sigma={}$'.format(sigma), loc='upper left')
+    ax.add_artist(anchored_text)
+    fig.savefig('{}_single_well_1d_{}.pdf'.format(datetime.now(), param), format='pdf')
+    plt.show()
+
+    return 0
+
 dim = 1
-positions = np.zeros((N, dim))
-a = 1
-eta = 1
-cont_maker = 1
-j = 0
-dim = 1
+N = 10000000
+eta = [np.sqrt(1.5)]
+a = [np.sqrt(1.5)]
+sigma = [1]
 
-#  Just one dimensional by now
+params = []
+for i in range(len(eta)):
+    params.append([eta[i], a[i], sigma[i]])
 
-
-for i in range(1, N):
-    noise = eta * np.random.normal(0, sigma)
-    positions[i] = positions[i - 1] + noise - eta * losses.grad_single_well_1d(positions[i - 1], a)
-
-
-x = np.linspace(-3 * sigma * np.sqrt(eta / a), 3 * sigma * np.sqrt(eta / a), 1000)
-p = np.zeros(len(x))
-for i in range(len(x)):
-    p[i] = utils.pdf_single_well_1d(x[i], sigma, eta, a)
-
-
-fig, ax = plt.subplots()
-fig.set_size_inches(3, 2)
-bins = 500
-ax.hist(positions, bins=bins, density=True)
-ax.plot(x, p, 'r')
-fig.savefig('prova.pdf', format='pdf')
-plt.show()
+for param in params:
+    dynamics_and_plot(param, N)
